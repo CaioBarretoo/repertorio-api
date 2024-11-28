d040973ae7161d0d7f8388476d9d6a3d00ac4fba
Caio Barreto <79175690+CaioBarretoo@users.noreply.github.com>
2024-11-28 13:00:05 -0300

    .

--- routes/repertorioRoutes.js
+++ routes/repertorioRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../firebase');
+const db = require('../database/firebase');
 
 // Rota para listar todas as músicas
 router.get('/', async (req, res) => {
