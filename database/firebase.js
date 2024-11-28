21919d98ebfcc8dec7e3833626c050b327df1239
Caio Barreto <79175690+CaioBarretoo@users.noreply.github.com>
2024-11-27 23:58:20 -0300

    Era ultima esperança

--- database/firebase.js
+++ database/firebase.js
@@ -1,9 +1,9 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../firebase-credentials.json');
 
-// Inicializar Firebase
+const firebaseCredentials = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(firebaseCredentials),
 });
 
 const db = admin.firestore();
