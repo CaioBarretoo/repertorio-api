d77c2171f3e0c5ff95039208cb680fa69967496b
Caio Barreto <79175690+CaioBarretoo@users.noreply.github.com>
2024-11-28 12:46:55 -0300

    Revert "Era ultima esperança"
    This reverts commit 21919d98ebfcc8dec7e3833626c050b327df1239.

--- database/firebase.js
+++ database/firebase.js
@@ -1,9 +1,9 @@
 const admin = require('firebase-admin');
+const serviceAccount = require('../firebase-credentials.json');
 
-const firebaseCredentials = JSON.parse(process.env.FIREBASE_CREDENTIALS);
-
+// Inicializar Firebase
 admin.initializeApp({
-  credential: admin.credential.cert(firebaseCredentials),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
